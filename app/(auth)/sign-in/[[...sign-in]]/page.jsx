9e5064ec10d17d233c03cc1b4634e2fc6faef6e9
Ishan-Saraf <ishaansaraf03@gmail.com>
2024-12-09 14:42:44 +0530

    Fixed issues in UI

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -1,5 +1,11 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignIn />
+  return (
+    <div>
+      <div className="flex justify-center items-center h-screen">
+        <SignIn />
+      </div>
+    </div>
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import { Button } from "@/components/ui/button";
-
- 
+import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
-      <Button>project</Button>
+      <h1>Welcome to the home page</h1>
+      <Button>Click me!</Button>
+
+      <UserButton />
     </div>
-  )
+  );
 }
